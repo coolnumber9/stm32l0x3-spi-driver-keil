d66d5968813a5bf52775e441841323ee222c757d
Kristoffer Dominic Amora <kdamora@gmail.com>
2019-02-14 04:29:20 +0800

    Initial bare bone template that compiles successfully

---
+++ Inc/main.h
@@ -0,0 +1,20 @@
+/*
+ * main.h
+ *
+ *      Author: coolnumber9
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define GPIO_BUTTON_PIN				0	/* Connected to PORT A Pin 0 */
+
+#define GPIO_PIN_13                	((uint16_t)0x2000U)  /* Pin 13 selected   */
+#define GPIO_PIN_14                	((uint16_t)0x4000U)  /* Pin 14 selected   */
+
+#define SWDIO_Pin 					GPIO_PIN_13
+#define SWDIO_GPIO_Port 			GPIOA
+#define SWCLK_Pin 					GPIO_PIN_14
+#define SWCLK_GPIO_Port 			GPIOA
+
+#endif /* MAIN_H_ */
---
+++ Src/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *      Author: coolnumber9
+ */
+
+#include "stm32l0x3_gpio_driver.h"
+//#include "led.h"
+#include "main.h"
+
+int main(void)
+{
+	
+	return 0;
+}
+
+
